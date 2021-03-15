5e6b8cb8b069a20e6019fbdaf64eb662ac0982f5
Lionel Beato <lionel.beato@gmail.com>
2021-03-15 12:11:45 -0400

    removes security files

--- src/main/java/com/tts/hosteddatabase/model/Volunteer.java
+++ src/main/java/com/tts/hosteddatabase/model/Volunteer.java
@@ -1,8 +1,5 @@
 package com.tts.hosteddatabase.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
